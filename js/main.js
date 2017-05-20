ff78af1ab1046ed249f28637c6bea7ea78b7a8b3
Ashwani Kumar <ashwanikumar31165@gmail.com>
2017-05-21 03:54:43 +0530

    Add: KNOCKOUT BINDINGS

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@ var global = {
     fs_url_start: "https:api.foursquare.com/v2/venues/",
     fs_url_end: "?client_id=4KJA1IBMXZKJF1SNSZGHVF3WVZOO5BC4BQOZ2GZD3CJMMBRQ&client_secret=XRJRIWXZDRVSV2MHVGLGBFWGJDMTKKQRGM00051EXN2DA5ER&v=20170514",
     fs_error: "Data Milla Nahi"
-    
+
 }
 //ViewModel function
 var model = function(){
   var self = this;
   self.rests = [];
+
   for(var i=0;i<rest.length;i++){
     var marker = new google.maps.Marker({
     position: {lat: rest[i].lat, lng: rest[i].lng},
@@ -18,7 +19,7 @@ var model = function(){
     animation: google.maps.Animation.DROP
     });
     self.rests.push(marker);
-    self.rests[self.rests.length - 1].setVisible(self.rests[self.rests.length - 1].show());  
+    self.rests[self.rests.length - 1].setVisible(self.rests[self.rests.length - 1].show());
     self.request = function(marker){
           $.ajax({
             type:"GET",
@@ -26,27 +27,63 @@ var model = function(){
             dataTypes: "json",
             success: function(data){
               var dat = data.response.venue;
-              if (dat.hasOwnProperty('likes')){ marker.likes = dat.likes.count; }else{ marker.likes = global.fs_error; } 
+              if (dat.hasOwnProperty('likes')){ marker.likes = dat.likes.count; }else{ marker.likes = global.fs_error; }
             },
             error : function(e){
               self.errorinfo(global.fs_error);
             }
         })
     }
-    
+
   }
 
 
-    for(var m=0;m<rest.length;m++){
+    for(var mar=0;mar<rest.length;mar++){
       (function(marker){
+
         self.request(marker);
         marker.addListener('click', function() {
           self.minfo(marker);
         });
-      })(self.rests[m]);
+      })(self.rests[mar]);
     }
-    
-    
+
+
+    self.setVisibilty = function(show){
+        for(var i = 0; i < rest.length; i++){
+            self.rests[i].show(show);
+        }
+    };
+
+    self.sText = ko.observable("");
+    self.searchIt = function(){
+      var sText = self.sText();
+      if((sText == "")||((sText.length === 0))){
+        self.setVisibilty(true);
+      }
+      else{
+        for(var mar = 0;mar<rest.length;mar++){
+          if(self.rests[mar]['title'].toLowerCase().indexOf(sText.toLowerCase()) >= 0){
+            self.rests[mar].show(true);
+            self.rests[mar].setVisible(true);
+          }
+          else{
+            self.rests[mar].show(false);
+            self.rests[mar].setVisible(false);
+          }
+        }
+      }
+    };
+
+
+    self.BounceMarker = function(marker){
+        self.dt = marker;
+        self.dt.setAnimation(google.maps.Animation.BOUNCE);
+		     setTimeout(function(){
+            self.dt.setAnimation(null);
+        }, 1000);
+    };
+
     self.minfo = function(marker) {
       self.dt = marker;
       formatData = function() {
@@ -59,9 +96,14 @@ var model = function(){
           }
       }
 
+
       //to display the likes of the restraunts
       var resinfo = "Name: " +self.dt.title+ "<br>" + formatData();
       markerdata.setContent(resinfo);
       markerdata.open(map, marker);
+      self.BounceMarker(marker);
+
     };
+
+
 };
